4b1a6cf63df4a14dc41db221bfc1f27a6100ce6d
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-10-22 18:01:19 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,9 +60,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-400/10`}
       >
-        <Providers>{children}
-      
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
