7a7803028813240e7b2962796ae621162bdc0130
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-10-22 20:17:01 +0300

    update

--- components/card/character-card.tsx
+++ components/card/character-card.tsx
@@ -6,7 +6,7 @@ function CharacterCard({ character }: { character: Character }) {
   return (
     <div className="flex flex-col mx-3 items-center justify-start bg-white border border-gray-200 rounded-lg shadow sm:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
       <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 md:rounded-none md:rounded-s-lg"
         src={character.image}
         alt={character.name}
       />
@@ -20,7 +20,7 @@ function CharacterCard({ character }: { character: Character }) {
           <div
             className={clsx(
               "h-4 w-4 rounded-full",
-              character.status === "Dead" ? "bg-red-700" : "bg-lime-600"
+              character.status === "Dead" && "bg-red-700" , character.status=="Alive"&&"bg-lime-600",character.status=="unknown"&&"bg-gray-600"
             )}
           />
           <p className="inline-block ml-2 text-sm text-gray-600">{character.status}-{character.species} </p>
@@ -33,8 +33,8 @@ function CharacterCard({ character }: { character: Character }) {
           </div>
 
           <div className="mt-1  flex flex-col">
-            <span className="text-gray-900 font-bold">Last known location:</span>
-            <span className="inline-block">{character.location.name}</span>
+            <span className="text-gray-900 font-bold">First seen in:</span>
+            <span className="inline-block">{character.origin.name}</span>
 
           </div>
          
