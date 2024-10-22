b2352f10b75f758e87dd677069a6b1c7d6e49d90
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-10-22 17:42:15 +0300

    update

--- components/header/header-title.tsx
+++ components/header/header-title.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function HeaderTitle() {
   return (
     <div className="flex justify-center my-10">
-        <h1 className="text-6xl ">The Rick and Morty API</h1>
+        <h1 className="text-3xl md:text-6xl ">The Rick and Morty API</h1>
     
       </div>
   )
